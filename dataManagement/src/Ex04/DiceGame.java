3dc8a2d9ca0f46b2bf226f929775def290d617b8
Ohnami Daichi <s1280045@u-aizu.ac.jp>
2021-07-08 12:49:43 +0900

    Initial revision

--- dataManagement/src/Ex04/DiceGame.java
+++ dataManagement/src/Ex04/DiceGame.java
@@ -1,10 +1,20 @@
 package Ex04;
 
+import java.util.Random;
+
 public class DiceGame {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int sum = 0;
+		int value;
+		System.out.println("Rolling the dice...");
+		Random r = new Random();
+		for(int i = 0; i < 2; i++) {
+			value = r.nextInt(6) + 1;
+			System.out.println("Die " + (i + 1) + ": " + value);
+			sum += value;
+		}
+		System.out.println("Total value: " + sum);
 	}
-
+	
 }
