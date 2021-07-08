6bf2c78edb5e8a373750e1c9d45b02b1b2efec7c
Ohnami Daichi <s1280045@u-aizu.ac.jp>
2021-07-08 17:09:22 +0900

    Greetings added

--- dataManagement/src/Ex04/DiceGame.java
+++ dataManagement/src/Ex04/DiceGame.java
@@ -1,12 +1,17 @@
 package Ex04;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		int sum = 0;
 		int value;
+		System.out.println("What is your name?");
+		String name = scan.next();
+		System.out.println("Hello," + name + "!");
 		System.out.println("Rolling the dice...");
 		Random r = new Random();
 		for(int i = 0; i < 2; i++) {
